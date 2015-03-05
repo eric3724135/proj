7550aee04d25a084576090977a976de2a73ff11e
Eric Wang <eric3724135@gmail.com>
2015-03-05 16:45:10 +0800

    add log4j

--- src/main/java/proj/controller/IndexConntroller.java
+++ src/main/java/proj/controller/IndexConntroller.java
@@ -1,6 +1,7 @@
 package proj.controller;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,19 +12,22 @@ import proj.service.ProductService;
 @Controller
 public class IndexConntroller {
 	
+	private static final Logger logger = Logger.getLogger(IndexConntroller.class);
+	
 	@Autowired
 	private ProductService productService;
 
 	@RequestMapping(value = "index")
 	public String index(Model m) {
-		System.out.println("index");
+		logger.debug("index");
+//		System.out.println("index");
 		m.addAttribute("products", productService.getProducts());
 		return "index";
 	}
 	
 	@RequestMapping("/")
 	public String index2(Model m) {
-		System.out.println("index");
+		logger.debug("index");
 		m.addAttribute("products", productService.getProducts());
 		return "index";
 	}
